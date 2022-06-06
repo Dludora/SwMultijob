79845548013e9735612fc6dadcfb7ec4e793f095
fish <2757188233@qq.com>
2022-06-06 19:22:22 +0800

    token

--- BlogWebsite/backend/models.py
+++ BlogWebsite/backend/models.py
@@ -2,8 +2,12 @@ from django.db import models
 class Author(models.Model):
     # Author表项，含用户名和密码，均为字符串属性，并设置最大长度
     username = models.CharField(max_length=50,primary_key=False)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=40)
     email = models.EmailField()
+    sex=models.CharField(max_length=5,null=True)
+    birthday=models.CharField(max_length=20,null=True)
+    discription=models.CharField(max_length=100,null=True)
+    lastLogin=models.CharField(max_length=50,null=True)
 class Article(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=100,null=True)
--- BlogWebsite/backend/urls.py
+++ BlogWebsite/backend/urls.py
@@ -2,12 +2,13 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register', register),  # 指定register函数的路由为register
-    path('login', login),
-    path('logout', logout),
-    path('delete',delete),
-    path('article',article_edit),
-    path('changeCondition',changeCondition),
-    path('changeUsername',changeUsername),
-    path('changePassword',changePassword)
+    path('register', register),
+    path('login', login),#若登录后重新登录，原令牌失效
+    path('logout', logout),#logout后，令牌失效
+    path('getSelfInformation',getSelfInformation),#输入token，查询下面五种个人信息，下面的修改信息均需传入token
+    path('setSelfUsername',setSelfUsername),#修改昵称，传入username
+    path('setSelfEmail',setSelfEmail),#修改邮箱，传入email
+    path('setSelfSex',setSelfSex),#修改性别，传入sex，只能是‘男’‘女’或‘未知’
+    path('setSelfBirthday',setSelfBirthday),#修改生日，传入birthday
+    path('setSelfDiscription',setSelfDiscription) #修改个人简介，传入discription
 ]
